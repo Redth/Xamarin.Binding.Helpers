a102cb15b9e9dbec7eca8e1ae57cbf20b6387684
Jonathan Dick <jondick@gmail.com>
2020-11-26 11:54:18 -0500

    Add code to fetch installed packages from a project

--- Xamarin.Binding.Helpers/NuGetUtil.cs
+++ Xamarin.Binding.Helpers/NuGetUtil.cs
@@ -10,6 +10,12 @@ using System.Threading.Tasks;
 using System.Threading;
 using NuGet.Versioning;
 using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Runtime.Versioning;
+using Newtonsoft.Json.Linq;
+using NuGet.Frameworks;
 
 namespace Xamarin.Binding.Helpers
 {
@@ -61,5 +67,81 @@ namespace Xamarin.Binding.Helpers
 
 			return null;
 		}
+
+		public static List<NuGetSuggestion> GetProjectPackages(string projectPath, string projectExtensionsPath, string targetFramework)
+			=> GetProjectPackagesConfigPackages(projectPath)
+				.Concat(GetProjectJsonPackages(projectExtensionsPath, targetFramework))
+				.ToList();
+
+		public static List<NuGetSuggestion> GetProjectPackagesConfigPackages(string projectPath)
+		{
+			var packages = new List<NuGetSuggestion>();
+
+			var path = Path.Combine(projectPath, "packages.config");
+
+			var xdoc = XDocument.Load(path);
+
+			var packageNodes = xdoc.XPathSelectElements("/packages/package");
+
+			foreach (var pkgNode in packageNodes)
+			{
+				var nugetId = pkgNode?.Attribute("id")?.Value;
+				var nugetVersion = pkgNode?.Attribute("version")?.Value;
+
+				if (!packages.Any(p => p.PackageId.Equals(nugetId, StringComparison.OrdinalIgnoreCase)))
+				{
+					packages.Add(new NuGetSuggestion
+					{
+						PackageId = nugetId,
+						Version = nugetVersion
+					});
+				}
+			}
+
+			return packages;
+		}
+
+		public static List<NuGetSuggestion> GetProjectJsonPackages(string projectExtensionsPath, string targetFramework)
+		{
+			var packages = new List<NuGetSuggestion>();
+
+			var path = Path.Combine(projectExtensionsPath, "project.assets.json");
+
+			JObject json = JObject.Parse(File.ReadAllText(path));
+
+
+			var tfm = NuGetFramework.Parse(targetFramework);
+
+			var targets = json?["targets"];
+			JToken projTarget = targets?[tfm.DotNetFrameworkName];
+
+			foreach (var rf in projTarget)
+			{
+				if (rf.First()["type"].Value<string>().Equals("package", StringComparison.OrdinalIgnoreCase))
+				{
+					var str = (rf as JProperty)?.Name;
+
+					var parts = str.Split(new[] { '/' }, 2);
+
+					if (parts != null && parts.Length == 2)
+					{
+						var nugetId = parts[0];
+						var nugetVersion = parts[1];
+
+						if (!packages.Any(p => p.PackageId.Equals(nugetId, StringComparison.OrdinalIgnoreCase)))
+						{
+							packages.Add(new NuGetSuggestion
+							{
+								PackageId = nugetId,
+								Version = nugetVersion
+							});
+						}
+					}
+				}
+			}
+
+			return packages;
+		}
+
 	}
 }
