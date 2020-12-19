62fa19789e34b6147d71c1e9f2b3e6a4316a5fe4
Jonathan Dick <jondick@gmail.com>
2020-12-18 21:40:23 -0500

    Revert test helper changes

--- test-helper.ps1
+++ test-helper.ps1
@@ -6,14 +6,7 @@ New-Item -ItemType Directory -Path .\output\ -Force
 Remove-Item .\Samples\packages\xamarin.binding.helpers -Force -Recurse
 Remove-Item C:\Users\jondi\.nuget\packages\Xamarin.Binding.Helpers -Force -Recurse
 
-Remove-Item 'C:\nuget\Xamarin.Binding.Helpers.0.2.0.nupkg'
-Copy-Item 'C:\xamarin\Xamarin.Binding.Helpers\output\Xamarin.Binding.Helpers.0.2.0.nupkg' 'C:\nuget\Xamarin.Binding.Helpers.0.2.0.nupkg'
-
 # Run restore/rebuilds
-# & msbuild /r /t:Rebuild .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj /bl:logs\android.binlog
-# & msbuild /t:Pack .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj /bl:logs\android-pack.binlog
-
-# & msbuild /r /t:Rebuild .\Samples\SampleAndroidApp\SampleAndroidApp.csproj /bl:logs\android-app.binlog
-
-
- & msbuild /r /t:Rebuild C:\code\MyMessageBridgeSample\MyMessageBridgeSample.Android\MyMessageBridgeSample.Android.csproj /bl
+& msbuild /r /t:Rebuild .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj /bl:logs\android.binlog
+& msbuild /t:Pack .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj /bl:logs\android-pack.binlog
+& msbuild /r /t:Rebuild .\Samples\SampleAndroidApp\SampleAndroidApp.csproj /bl:logs\android-app.binlog
