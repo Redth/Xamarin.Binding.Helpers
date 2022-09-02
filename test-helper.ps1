a3cb439fb4cef956841c7b4362cf612f9adfb3b6
redth <jondick@gmail.com>
2022-09-01 20:19:34 -0400

    Cleanup for mac

--- test-helper.ps1
+++ test-helper.ps1
@@ -1,12 +1,11 @@
 New-Item -ItemType Directory -Path .\logs\ -Force
 New-Item -ItemType Directory -Path .\output\ -Force
 
-& msbuild /r /t:Rebuild .\Xamarin.Binding.Helpers\Xamarin.Binding.Helpers.csproj /bl:logs\xbh.binlog
+& dotnet build -t:Rebuild .\Xamarin.Binding.Helpers\Xamarin.Binding.Helpers.csproj -bl:logs/xbh.binlog
 
-Remove-Item .\Samples\packages\xamarin.binding.helpers -Force -Recurse
-Remove-Item C:\Users\jondi\.nuget\packages\Xamarin.Binding.Helpers -Force -Recurse
+Remove-Item .\Samples\packages\ -Force -Recurse
 
 # Run restore/rebuilds
-& msbuild /r /t:Rebuild .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj /bl:logs\android.binlog
-& msbuild /t:Pack .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj /bl:logs\android-pack.binlog
-& msbuild /r /t:Rebuild .\Samples\SampleAndroidApp\SampleAndroidApp.csproj /bl:logs\android-app.binlog
+& dotnet build -t:Rebuild .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj -bl:logs/android.binlog
+& dotnet build -t:Pack .\Samples\SampleAndroidBinding\SampleAndroidBinding.csproj -bl:logs/android-pack.binlog
+& dotnet build -t:Rebuild .\Samples\SampleAndroidApp\SampleAndroidApp.csproj -bl:logs/android-app.binlog
